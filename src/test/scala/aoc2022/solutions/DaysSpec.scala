14dadd383d3173db4db9406662addb9159527076
Anton Ivanov <anton.al.ivanov@gmail.com>
2022-12-03 01:31:48 +0300

    Spec for Day 2

--- src/test/scala/aoc2022/solutions/DaysSpec.scala
+++ src/test/scala/aoc2022/solutions/DaysSpec.scala
@@ -11,3 +11,11 @@ class DaysSpec extends munit.FunSuite:
     assertEquals(solutionPart1(parsed), 24000)
     assertEquals(solutionPart2(parsed), 45000)
   }
+
+  test("Day 2") {
+    import Day2._
+    import Day2Input._
+    val parsed = parse(input)
+    assertEquals(solutionPart1(parsed), 15)
+    assertEquals(solutionPart2(parsed), 12)
+  }
