6101dd6ee706dd583addc73d3dc82474d90cf16f
Anton Ivanov <anton.al.ivanov@gmail.com>
2022-12-11 23:39:58 +0300

    Implement parsing for Day 11

--- src/main/scala/aoc2022/solutions/Day11.scala
+++ src/main/scala/aoc2022/solutions/Day11.scala
@@ -1,5 +1,7 @@
 package aoc2022.solutions
 
+import aoc2022.solutions.common.ParsingUtils._
+
 object Day11:
 
   case class Monkey(
@@ -11,8 +13,37 @@ object Day11:
     monkeyIdIfNotDivisible: Int
   )
 
-  def parse(input: String): List[Monkey] =
-    ???
+  def parse(input: String): Seq[Monkey] =
+    val splitInput = splitBy("")(input.split("\n").map(_.trim)).filter(_.size > 0)
+    splitInput.map(parseMonkey)
+
+  def parseMonkey(input: Seq[String]): Monkey =
+    val Seq(idInput, itemsInput, operationInput, testInput, ifTrueInput, ifFalseInput) = input
+    val id = idInput match {
+      case s"Monkey $id:" =>
+        id.toInt
+    }
+    val initialItems = itemsInput match {
+      case s"Starting items: $items" =>
+        items.split(",").map(_.trim.toInt).toList
+    }
+    val operation = operationInput match {
+      case s"Operation: $operation" =>
+        parseMonkeyFunction(operation)
+    }
+    val divisibleBy = testInput match {
+      case s"Test: divisible by $value" =>
+        value.toInt
+    }
+    val monkeyIdIfDivisible = ifTrueInput match {
+      case s"If true: throw to monkey $id" =>
+        id.toInt
+    }
+    val monkeyIdIfNotDivisible = ifFalseInput match {
+      case s"If false: throw to monkey $id" =>
+        id.toInt
+    }
+    Monkey(id, initialItems, operation, divisibleBy, monkeyIdIfDivisible, monkeyIdIfNotDivisible)
 
   def parseMonkeyFunction(input: String): Int => Int =
     input match {
