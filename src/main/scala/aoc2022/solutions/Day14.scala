3422c61ba3d70303ae0b05f46fdc7534448a145b
Anton Ivanov <anton.al.ivanov@gmail.com>
2022-12-27 23:37:55 +0300

    Add tests for line, rocktrace and simulation state contains

--- src/main/scala/aoc2022/solutions/Day14.scala
+++ src/main/scala/aoc2022/solutions/Day14.scala
@@ -4,7 +4,7 @@ import scala.annotation.tailrec
 
 object Day14:
 
-  case class SimulationState(rockTraces: List[RockTrace], stoppedSand: List[Point] = List()):
+  case class SimulationState(rockTraces: List[RockTrace], stoppedSand: List[Point] = List(), freeFallingSand: Boolean = false):
     def isBlocked(p: Point): Boolean =
       stoppedSand.contains(p) || rockTraces.exists(_.contains(p))
 
@@ -13,11 +13,19 @@ object Day14:
       List(Point(x, y + 1), Point(x - 1, y + 1), Point(x + 1, y + 1))
 
   case class Line(start: Point, end: Point):
+    private val minX = Math.min(start.x, end.x)
+    private val maxX = Math.max(start.x, end.x)
+    private val minY = Math.min(start.y, end.y)
+    private val maxY = Math.max(start.y, end.y)
     def contains(p: Point): Boolean =
       val Point(x1, y1) = start
       val Point(x2, y2) = end
       // Easy to check that start and end belong to the line with this equation => this is the equation of this line
       p.y * (x2 - x1) == p.x * (y2 - y1) + (y1 * x2 - y2 * x1)
+      &&
+        ((p.x >= minX) && (p.x <= maxX))
+      &&
+        ((p.y >= minY) && (p.y <= maxY))
 
   case class RockTrace(points: List[Point]):
     val lines = points.zip(points.tail).map((start, end) => Line(start, end))
--- src/test/scala/aoc2022/solutions/Day14Spec.scala
+++ src/test/scala/aoc2022/solutions/Day14Spec.scala
@@ -1,12 +1,52 @@
 package aoc2022.solutions
 
-import aoc2022.solutions.Day14.{Point, RockTrace, SimulationState, dropSand}
+import aoc2022.solutions.Day14.{Point, Line, RockTrace, SimulationState, dropSand}
 
 class Day14Spec extends munit.FunSuite:
 
-  //TODO: Tests for Line contains
-  //TODO: Tests for RockTrace contains
-  //TODO: Tests for SimulationState contains
+  test("Line contains") {
+    val verticalLine = Line(Point(2, 1), Point(2, 6))
+    assert(verticalLine.contains(Point(2, 1)))
+    assert(verticalLine.contains(Point(2, 4)))
+    assert(verticalLine.contains(Point(2, 6)))
+    assert(!verticalLine.contains(Point(1, 1)))
+    assert(!verticalLine.contains(Point(2, 10)))
+    assert(!verticalLine.contains(Point(10, 12)))
+
+    val horizontalLine = Line(Point(10, 2), Point(12, 2))
+    assert(horizontalLine.contains(Point(10, 2)))
+    assert(horizontalLine.contains(Point(12, 2)))
+    assert(horizontalLine.contains(Point(11, 2)))
+    assert(!horizontalLine.contains(Point(9, 1)))
+    assert(!horizontalLine.contains(Point(13, 2)))
+    assert(!horizontalLine.contains(Point(1, 2)))
+  }
+
+  test("RockTrace contains") {
+    val rockTrace = RockTrace(List(Point(2, 1), Point(2, 4), Point(7, 4)))
+    assert(rockTrace.contains(Point(2, 1)))
+    assert(rockTrace.contains(Point(2, 2)))
+    assert(rockTrace.contains(Point(2, 4)))
+    assert(rockTrace.contains(Point(5, 4)))
+    assert(rockTrace.contains(Point(7, 4)))
+    assert(!rockTrace.contains(Point(4, 2)))
+    assert(!rockTrace.contains(Point(1, 5)))
+  }
+
+  test("SimulationState isBlocked") {
+    val rockTraces = List(
+      RockTrace(List(Point(2, 1), Point(2, 4), Point(7, 4))),
+      RockTrace(List(Point(3, 2), Point(5, 2)))
+    )
+    val stoppedSand = List(Point(3, 1), Point(4, 1), Point(5, 1))
+    val simulationState = SimulationState(rockTraces, stoppedSand)
+    assert(simulationState.isBlocked(Point(4, 4)))
+    assert(simulationState.isBlocked(Point(7, 4)))
+    assert(simulationState.isBlocked(Point(4, 1)))
+    assert(!simulationState.isBlocked(Point(4, 5)))
+    assert(!simulationState.isBlocked(Point(6, 1)))
+    assert(!simulationState.isBlocked(Point(4, 3)))
+  }
 
   test("dropSand: rock trace bottom a few points below") {
     val state = SimulationState(List(RockTrace(List(Point(0, 10), Point(5, 10)))))
@@ -18,4 +58,8 @@ class Day14Spec extends munit.FunSuite:
 
   test("dropSand: examples similar to examples from the task") {
     //TODO:
+  }
+
+  test("dropSand: sand drops infinitely to the bottom") {
+
   }
