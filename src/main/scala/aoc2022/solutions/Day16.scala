d97454f41dc7a4953b3729407ed440be9ce355f5
Anton Moilanen <anton.a.moilanen@gmail.com>
2023-02-06 00:26:02 +0300

    Initial sketch of the algorithm for part 1 of Day 16, a variation of Bellman-Ford

--- src/main/scala/aoc2022/solutions/Day16.scala
+++ src/main/scala/aoc2022/solutions/Day16.scala
@@ -1,6 +1,8 @@
 package aoc2022.solutions
 
+import aoc2022.solutions.Day12.Vertex
 import aoc2022.solutions.common.ParsingUtils.ParsingError
+import scala.collection.mutable.{ Map => MutableMap }
 
 object Day16:
 
@@ -21,6 +23,55 @@ object Day16:
 
   def parse(input: String): Seq[Valve] =
     input.split("\n").map(_.trim).filter(_.nonEmpty).map(parseValve).toSeq
+
+  case class ValveMaxPressureAndOpenValves(pressure: Int, openValves: Set[ValveId])
+
+  def findGreatestPressure(valves: Seq[Valve], totalMoveNumber: Int): Int =
+    // Partially computed max possible pressures and the sets of open valves for each valve
+    val state: MutableMap[ValveId, MutableMap[Int, ValveMaxPressureAndOpenValves]] = MutableMap.empty
+    valves.foreach(valve =>
+      state(valve.id) = MutableMap.from(Map(totalMoveNumber -> ValveMaxPressureAndOpenValves(0, Set.empty)))
+    )
+
+    def improveNextValveAchievedPressures(updatedMoveNumber: Int, updatedPressure: Int, updatedOpenValves: Set[ValveId], nextValveIds: Seq[ValveId]): Unit =
+      nextValveIds.foreach(nextValveId =>
+        val nextValveState = state(nextValveId)
+        if nextValveState.contains(updatedMoveNumber) then
+          val currentNextValveBestPressure = nextValveState(updatedMoveNumber)
+          if currentNextValveBestPressure.pressure < updatedPressure then
+            nextValveState(updatedMoveNumber) = ValveMaxPressureAndOpenValves(updatedPressure, updatedOpenValves)
+        else
+          nextValveState(updatedMoveNumber) = ValveMaxPressureAndOpenValves(updatedPressure, updatedOpenValves)
+      )
+
+    (1 to totalMoveNumber).foreach(_ =>
+      valves.foreach(valve =>
+        val valveState = state(valve.id)
+        valveState.foreach({ case (moveNumber, ValveMaxPressureAndOpenValves(pressure, openValves)) =>
+          val nextValveIds = valve.connections
+          // First compute the improved estimates for the case when the current valve is open (if it is not already open)
+          if !openValves.contains(valve.id) then
+            val pressureIncreasedIfTurnedOn = (moveNumber - 1) * valve.rate // the pressure the current open valve will release until the moves end
+            improveNextValveAchievedPressures(
+              updatedMoveNumber = moveNumber - 2, // 1 move to open the current valve + 1 move to move to another valve
+              updatedPressure = pressure + pressureIncreasedIfTurnedOn,
+              updatedOpenValves = openValves + valve.id,
+              nextValveIds = nextValveIds
+            )
+          // Compute the improved estimates for the case when the current valve is not open
+          improveNextValveAchievedPressures(
+            updatedMoveNumber = moveNumber - 1, // 1 move to move to another valve
+            updatedPressure = pressure,
+            updatedOpenValves = openValves,
+            nextValveIds = nextValveIds
+          )
+        })
+      )
+    )
+
+    //TODO: Iterate through the valve states and see whether some valves contain move number 0 and what would be the best possible estimate
+    //among all those achieved 0s
+    ???
 
 @main
 def day16Main: Unit =
